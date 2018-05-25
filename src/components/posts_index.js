84bdeee55981fff2811709a1bfbd664431cb7ca1
Lee <Z245647@caremarkrx.net>
2018-05-25 14:22:02 -0400

    booo

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
-    console.log('End of fetchPosts');
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return <li key={post.id}>{post.title}</li>;
+    });
+  };
+
   render() {
-    console.log(this.props.posts);
     return (
       <div>
-        Posts:
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   };
